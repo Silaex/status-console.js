2fecc6359a499d5140f4588b01c13ae40ee7a4ff
Silaex <alexis.provot45@gmail.com>
2020-03-04 03:26:06 +0100

    created success, error, warn and info status console

---
+++ index.js
@@ -0,0 +1,8 @@
+require('colors');
+const print = require('./src/statusConsole');
+
+print.info('salut YO YOY OYOYO OYOY ', 'index.js', '400')
+print.error('salut YO YOY OYOYO OYOY', 'index.js', '400')
+print.warn('salut YO YOY OYOYO OYOY ', 'index.js', '400')
+print.success(55)
+
